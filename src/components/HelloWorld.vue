58ddcabfe198625209d3c815ce6453f9bf09011b
hibana2077 <hibana2077@gmail.com>
2023-03-26 12:32:41 +0800

    2023-03-26

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2023-03-26 07:30:12
  * @LastEditors: hibana2077 hibana2077@gmail.com
- * @LastEditTime: 2023-03-26 11:53:33
+ * @LastEditTime: 2023-03-26 12:32:29
  * @FilePath: \vuetify-project\src\components\HelloWorld.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -121,9 +121,3 @@ onMounted(() => {
 });
 
 </script>
-
-<style lang="scss" scoped>
-  .v-btn.active .v-icon {
-    transform: rotate(360deg);
-  }
-</style>
