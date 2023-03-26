b7506e01998936c7421c852dd1332337de185e96
hibana2077 <hibana2077@gmail.com>
2023-03-26 21:45:14 +0800

    2023-03-26

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -2,14 +2,14 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2023-03-26 07:30:12
  * @LastEditors: hibana2077 hibana2077@gmail.com
- * @LastEditTime: 2023-03-26 21:32:52
+ * @LastEditTime: 2023-03-26 21:43:42
  * @FilePath: \vuetify-project\src\layouts\default\AppBar.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
   <v-app-bar :color="pr">
     <v-app-bar-title>
-      <v-avatar image="../../assets/nttu_hack.svg" size="35"></v-avatar>
+      <v-avatar image="../../assets/nttu_hack_without_back.svg" size="35"></v-avatar>
 
       東大資安社
     </v-app-bar-title>
