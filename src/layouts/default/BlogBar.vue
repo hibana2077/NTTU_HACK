45a061e0a546ae0b02c382650bc3d5d8cedeaeed
hibana2077 <hibana2077@gmail.com>
2023-03-26 12:30:17 +0800

    2023-03-26

---
+++ src/layouts/default/BlogBar.vue
@@ -0,0 +1,39 @@
+<!--
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2023-03-26 07:30:12
+ * @LastEditors: hibana2077 hibana2077@gmail.com
+ * @LastEditTime: 2023-03-26 12:23:43
+ * @FilePath: \vuetify-project\src\layouts\default\AppBar.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+    <v-app-bar :color="pr">
+    
+      <v-app-bar-title>
+        
+        <v-icon icon="mdi-redhat" />
+  
+        東大資安社 部落格
+        
+      </v-app-bar-title>
+      <v-spacer></v-spacer>
+        <v-btn icon>
+            <RouterLink to="/" class="text-decoration-none" style="color: inherit;">
+                <v-icon icon="mdi-home" />
+            </RouterLink>
+        </v-btn>
+        <v-btn icon>
+            <v-icon icon="mdi-account" />
+        </v-btn>
+        <v-btn icon>
+            <v-icon icon="mdi-bell" />
+        </v-btn>
+    </v-app-bar>
+</template>
+  
+<script setup>
+  import { ref, onMounted } from 'vue'
+  
+  const pr = ref('primary')
+</script>
+  
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2023-03-26 07:30:12
  * @LastEditors: hibana2077 hibana2077@gmail.com
- * @LastEditTime: 2023-03-26 10:38:58
+ * @LastEditTime: 2023-03-26 12:14:34
  * @FilePath: \vuetify-project\src\router\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -23,17 +23,23 @@ const routes = [
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
       },
       {
-        path: 'blog',
-        name: 'Blog',
-        component: () => import(/* webpackChunkName: "blog" */ '@/views/Blog.vue'),
-      },
-      {
         path: 'about',
         name: 'About',
         component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue'),
       }
     ],
   },
+  {
+    path: '/blog',
+    component: () => import('@/layouts/default/Default_blog.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Blog',
+        component: () => import(/* webpackChunkName: "blog" */ '@/views/Blog.vue'), 
+      },
+    ],
+  }
 ]
 
 const router = createRouter({
