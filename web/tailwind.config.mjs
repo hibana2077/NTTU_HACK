15979773ede0e45daa4cc287cfe0d38834b05550
hibana2077 <hibana2077@gmaill.com>
2024-02-26 11:00:34 +0800

    Update Footer and Navbar styles

--- web/tailwind.config.mjs
+++ web/tailwind.config.mjs
@@ -1,8 +1,8 @@
 /*
  * @Author: hibana2077 hibana2077@gmail.com
  * @Date: 2024-02-25 08:56:24
- * @LastEditors: hibana2077 hibana2077@gmail.com
- * @LastEditTime: 2024-02-25 08:57:33
+ * @LastEditors: hibana2077 hibana2077@gmaill.com
+ * @LastEditTime: 2024-02-26 09:51:53
  * @FilePath: \NTTU_HACK\web\tailwind.config.mjs
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
